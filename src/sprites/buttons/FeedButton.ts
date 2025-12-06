ec53aca3325cdc831fafc172698aae30164fde01
Mariano Hurtado de Mendoza Carranza <mariano.hmc@hotmail.com>
2025-12-05 19:38:37 -0600

    new game

--- src/sprites/buttons/FeedButton.ts
+++ src/sprites/buttons/FeedButton.ts
@@ -1,16 +1,17 @@
 import Phaser from "phaser";
 import { normalize } from "../../utilities/normalizing";
+import type { MainScene } from "../../scenes/MainScene";
 
 export class FeedButton extends Phaser.GameObjects.Image {
 
-  constructor(scene:Phaser.Scene, x:number, y:number) {
+  constructor(scene:MainScene, x:number, y:number) {
     super(scene, x, y, "feed-button");
     scene.add.existing(this);
 
     this.setInteractive({useHandCursor: true});
     
     this.scale = normalize(this.scale);
-    
+
     this.on("pointerdown", () => {
       console.log("Eat!");
     });
