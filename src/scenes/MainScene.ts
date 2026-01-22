d58a1d74eecbf4eb672a5fa1d6e9eab59119a686
Mariano Hurtado de Mendoza Carranza <mariano.hmc@hotmail.com>
2026-01-22 17:22:42 -0600

    Changes

--- src/scenes/MainScene.ts
+++ src/scenes/MainScene.ts
@@ -2,10 +2,12 @@ import Phaser from "phaser";
 import { Pet } from "../sprites/entities/Pet";
 import { FeedButton } from "../sprites/buttons/FeedButton";
 import type { GameManagerScene } from "./GameManager";
+import { InteractButton } from "../sprites/buttons/InteractButton";
 
 export class MainScene extends Phaser.Scene {
   private _pet!: Pet;
   private _feedButton!: FeedButton;
+  private _interactButton!: InteractButton;
 
   constructor() {
     super({key: "main-scene"});
@@ -34,13 +36,27 @@ export class MainScene extends Phaser.Scene {
 
     //Change position of feedButton
     this.feedButton.x = 8;
-    this.feedButton.y = 3 * (this.game.config.width as number) / 4;
+    this.feedButton.y = 3 * (this.game.config.height as number) / 4;
 
     //Create event on "feed-pet": set state first, apply state to pet
     this.events.on("feed-pet", () => {
       manager.petState.health = this.pet.petState.health + 1;
       this.pet.applyState(manager.petState); 
       console.log(this.pet.petState);
+    });
+
+    //Generate interactButton
+    this.interactButton = new InteractButton(this, 0, 0);
+
+    //Change origin to right corner
+    this.interactButton.setOrigin(1,0);
+
+    //Change position of interactButton
+    this.interactButton.x = (this.game.config.width as number) - 8;
+    this.interactButton.y = 3 * (this.game.config.height as number) / 4;
+
+    this.events.on("interact-toggle", () => {
+      console.log("clicked interaction page");
     });
   }
 
@@ -60,7 +76,17 @@ export class MainScene extends Phaser.Scene {
   }
   
   //set feedButton
-  set feedButton(fb: FeedButton){
+  set feedButton(fb: FeedButton) {
     this._feedButton = fb;
+  }
+
+  //get interactButton
+  get interactButton(): InteractButton {
+    return this._interactButton;
+  }
+
+  //set interactButton
+  set interactButton(ib: InteractButton) {
+    this._interactButton = ib;
   }
 }
